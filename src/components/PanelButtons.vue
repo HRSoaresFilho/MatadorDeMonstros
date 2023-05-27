80b14695d6e1841d6a45466ab52bdb9c61c03602
Humberto Filho <hr.soaresfilho@gmail.com>
2023-05-26 23:12:08 -0300

    Criação dos botões

--- src/components/PanelButtons.vue
+++ src/components/PanelButtons.vue
@@ -1,11 +1,30 @@
-<script setup>
+<script>
+import BaseButton from './buttons/BaseButton.vue';
+export default {
+    data() {
+        return {
 
+        }
+    },
+    components: {
+        BaseButton
+    }
+}
 </script>
 
 <template>
-<div class="panel">Botões</div>
+    <div class="panel buttons">
+        <BaseButton name="Ataque" classBtn="attack" />
+        <BaseButton name="Ataque Especial" classBtn="especial-attack" />
+        <BaseButton name="Curar" classBtn="heal" />
+        <BaseButton name="Desistir" classBtn="give-up" />
+        <BaseButton name="Iniciar Jogo" classBtn="new-game" />
+    </div>
 </template>
-
 <style scoped>
+.buttons{
+    display: flex;
+    justify-content: center;
 
+}
 </style>
---
+++ src/components/buttons/BaseButton.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        classBtn: {
+            type: String,
+            required: false
+        }
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
+<template>
+    <button class="btn" :class="classBtn">
+        {{ name }}
+    </button>
+</template>
+<style scoped>
+.btn {
+    padding: 8px 10px;
+    margin: 0px 10px;
+    border: none;
+    text-transform: uppercase;
+    font-size: 1.1rem;
+}
+
+.new-game {
+    background-color: #4253af;
+    color: #fff;
+}
+
+.attack {
+    background-color: #e51c23;
+    color: #fff;
+}
+
+.especial-attack{
+    background-color: #ff9800;
+    color: #000;
+}
+
+.heal{
+    background-color: #259b24;
+    color: #fff;
+}
+
+.give-up{
+    background-color: #bbb;
+    color: #000;
+}
+</style>
