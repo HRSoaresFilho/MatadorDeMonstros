d3da2e4ab588b30ee969bc0ab8a9c318a515ed68
Humberto Filho <hr.soaresfilho@gmail.com>
2023-05-29 06:27:21 -0300

    Implementação do estilo do log

--- src/components/PanelLogs.vue
+++ src/components/PanelLogs.vue
@@ -20,15 +20,41 @@ export default {
 </script>
 
 <template>
-<div v-if="logs.length" class="panel logs">
-    <ul>
-        <li v-for="log in logs" :key="log.id" class="log">
-            {{ log.text }}
-        </li>
-    </ul>
-</div>
+    <div v-if="logs.length" class="panel logs">
+        <ul>
+            <li v-for="log in logs" :key="log.id" class="log" :class="log.cls">
+                {{ log.text }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
+.logs ul {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
 
+.logs ul li {
+    display: flex;
+    justify-content: center;
+    margin: 4px 0px;
+    padding: 3px 0px;
+    font-weight: 600;
+    font-size: 1.1rem;
+    text-transform: uppercase;
+}
+
+.player{
+    background-color: #4253afaa;
+    color: #fff;
+}
+
+.monster{
+    background-color: #e51c23aa;
+    color: #fff;
+}
 </style>
